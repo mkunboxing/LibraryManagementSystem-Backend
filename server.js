bf1ecce329bd2a58ba49b0f2b6a0a4eb48acf79b
Mukul Kumar <mkunboxing@gmail.com>
2024-06-02 21:01:21 +0530

    pushed cors

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const corsOptions = {
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(cookieSession({
     name: 'lms-session',
