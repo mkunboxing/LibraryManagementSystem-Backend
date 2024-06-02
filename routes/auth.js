f57f55481053a284ff8c4b6cad783972a99ea1a8
Mukul Kumar <mkunboxing@gmail.com>
2024-06-02 20:15:39 +0530

    pushed

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,7 @@ const User = require("../models/user");
 
 
 router.get("/login/success", async (req, res) => {
+    passport.authenticate("google", { failureRedirect: "/login/filled" });
     console.log("/login/success route",req.user)
     if (req.user) {
         const user = req.user._json
